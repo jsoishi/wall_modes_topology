fe1ed6e57a42794ec9a9fac099a0858da3ea6cfb
J. S. Oishi <jsoishi@gmail.com>
2020-10-31 11:21:45 -0700

    updated merge script

--- python/merge.sh
+++ python/merge.sh
@@ -1,5 +1,5 @@
 echo "running with $1 cores"
 conda activate dedalus-dev
-mpirun -np $1 python3 -m dedalus merge_procs top_slices
-mpirun -np $1 python3 -m dedalus merge_procs side_slices
-mpirun -np $1 python3 -m dedalus merge_procs mid_slices
+mpirun -np $1 python3 merge.py $2/slices --cleanup
+mpirun -np $1 python3 merge.py $2/checkpoints --cleanup
+mpirun -np $1 python3 merge.py $2/data --cleanup
